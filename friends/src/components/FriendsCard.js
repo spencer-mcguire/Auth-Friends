e307d78d67523acb16856bca5bc2b43cf5e6837f
spencer-mcguire <smcguire1826@gmail.com>
2019-12-17 15:25:22 -0600

    get request in private route

--- friends/src/components/FriendsCard.js
+++ friends/src/components/FriendsCard.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 export const FriendsCard = props => {
+  const { name, age, email } = props.data;
   return (
     <div>
-      <h3>name</h3>
-      <p>age</p>
-      <p>email</p>
+      <h3>{name}</h3>
+      <p>{age}</p>
+      <p>{email}</p>
     </div>
   );
 };
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 import { FriendsCard } from "./FriendsCard";
 
 const FriendsList = () => {
+  const [friends, setFriends] = useState([]);
+  console.log(friends);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axiosWithAuth()
+      .get("/friends")
+      .then(res => setFriends(res.data))
+      .then(err => console.log(err));
+  };
+
   return (
     <div>
-      <FriendsCard />
+      {friends.length === 0 && <p>loading friends</p>}
+      {friends.map(i => (
+        <FriendsCard key={i.id} data={i} />
+      ))}
     </div>
   );
 };
